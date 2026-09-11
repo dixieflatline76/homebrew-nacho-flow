676a4d3726e90b18bcfd9118971404a499944d20
Karl <karl.kwong@spicebox.dev>
2026-09-11 22:11:51 +0200

    chore: update Formula for v1.1.0

--- Formula/nacho-flow.rb
+++ Formula/nacho-flow.rb
@@ -1,28 +1,28 @@
 class NachoFlow < Formula
   desc "Agent Supervisor & Model Dispatcher for coding agents"
   homepage "https://spicebox.dev/nacho-flow/"
-  version "1.0.2"
+  version "1.1.0"
   license "AGPL-3.0-or-later"
 
   on_macos do
     on_arm do
       url "https://github.com/dixieflatline76/nacho-flow/releases/download/v#{version}/nacho-flow-#{version}-darwin-arm64"
-      sha256 "5dedffdc843c5c582780097ce4cab008cbf6795fe9c5c45dc298b5409fdec5d1"
+      sha256 "c8694be8b3c651207432a6c5b94208c740c77e30275724a61a49f2e310d99619"
     end
     on_intel do
       url "https://github.com/dixieflatline76/nacho-flow/releases/download/v#{version}/nacho-flow-#{version}-darwin-amd64"
-      sha256 "c6ca494c78aa449bcb029d0fee9d9ba370eaf928abf67e98092c15f81f962e1b"
+      sha256 "846fcbac03944a3e962546c3a40e149985b031ec4b1fb01a96ae90bc550e256a"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/dixieflatline76/nacho-flow/releases/download/v#{version}/nacho-flow-#{version}-linux-amd64"
-      sha256 "beecdc60da521d18d35c9c9731492a4a21579988d8ce7cc9a70fd62bc0cb886e"
+      sha256 "cac9fd4b8aba900d0ec19db8d098b200d1882352496a78dc200d4d12c6efda9e"
     end
     on_arm do
       url "https://github.com/dixieflatline76/nacho-flow/releases/download/v#{version}/nacho-flow-#{version}-linux-arm64"
-      sha256 "66ee07597bb8b665ee7d5310bf043d84399ad32286cc659a164aad7e7cbc224a"
+      sha256 "3748cee50f3987ea3b8a3ca93519b0d9ba6c27e91e749b72c78fd1a81ac54529"
     end
   end
 
